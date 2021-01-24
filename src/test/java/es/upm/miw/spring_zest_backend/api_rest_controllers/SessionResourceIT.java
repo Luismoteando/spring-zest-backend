3eaed7333b0ce3914c90134eb6fd6764d25d739b
Luis Miguel Ortiz Rozalén <luismiguel.ortiz.rozalen@alumnos.upm.es>
2021-01-24 22:33:27 +0100

    Edit a session API endpoint #10

--- src/test/java/es/upm/miw/spring_zest_backend/api_rest_controllers/SessionResourceIT.java
+++ src/test/java/es/upm/miw/spring_zest_backend/api_rest_controllers/SessionResourceIT.java
@@ -4,6 +4,7 @@ import es.upm.miw.spring_zest_backend.documents.SessionExercise;
 import es.upm.miw.spring_zest_backend.dtos.SessionCreationDto;
 import es.upm.miw.spring_zest_backend.dtos.SessionDto;
 import es.upm.miw.spring_zest_backend.repositories.SessionExerciseRepository;
+import es.upm.miw.spring_zest_backend.repositories.SessionRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static es.upm.miw.spring_zest_backend.api_rest_controllers.SessionResource.ID;
 import static es.upm.miw.spring_zest_backend.api_rest_controllers.SessionResource.SESSIONS;
 
 @ApiTestConfig
@@ -22,6 +24,9 @@ public class SessionResourceIT {
 
     @Autowired
     private WebTestClient webTestClient;
+
+    @Autowired
+    private SessionRepository sessionRepository;
 
     @Autowired
     private SessionExerciseRepository sessionExerciseRepository;
@@ -55,4 +60,20 @@ public class SessionResourceIT {
                 .value(Assertions::assertNotNull);
     }
 
+    @Test
+    void testUpdateSession() {
+        String id = this.sessionRepository.findAll().get(0).getId();
+        List<String> sessionExerciseIds = this.sessionExerciseRepository.findAll()
+                .stream()
+                .map(SessionExercise::getId)
+                .collect(Collectors.toList());
+        this.webTestClient
+                .put().uri(contextPath + SESSIONS + ID, id)
+                .body(
+                        BodyInserters.fromObject(
+                                new SessionCreationDto("Stretching day A", new Date(), sessionExerciseIds)
+                        )
+                ).exchange().expectStatus().isOk().expectBody(SessionDto.class)
+                .value(Assertions::assertNotNull);
+    }
 }
