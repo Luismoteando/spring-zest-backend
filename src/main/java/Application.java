269cff3be5f877a32d4d3712b7ecef7b249929c8
Luis Miguel Ortiz Rozalén <luismiguel.ortiz.rozalen@alumnos.upm.es>
2021-01-24 10:01:16 +0100

    Add the main class

---
+++ src/main/java/Application.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class}) // Not: /error
+public class Application {
+
+    // mvn clean spring-boot:run
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
